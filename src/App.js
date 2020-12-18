31f78205c50f96461ab4a6e5bbf17c43e2e1c12b
Andrew Rogerson <andrew.rogerson@free-now.com>
2020-12-18 16:19:49 +0100

    setup css modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './index.css';
 
 const App = ({ title }) => (
-  <div>
+  <div className={styles.title}>
     <h1>{title}</h1>
   </div>
 );
