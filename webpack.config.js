d2b3370ddb6088df6988dcd68adfadcb57bae5b1
Andrew Rogerson <andrew.rogerson@free-now.com>
2020-12-18 14:56:51 +0100

    setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,20 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
   output: {
-    path: path.resolve(__dirname, '/dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
   devServer: {
